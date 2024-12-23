7744730739a8dc9f93fb9f4a2394ab856028fdba
Your Name <golamsarwar1896@gmail.com>
2024-12-24 04:07:07 +0600

    Implemented the delete method. Now it will delete posts from the ui and also remove it from the db

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -34,6 +34,22 @@ async function run() {
     //Showing Queries in the client site
     app.get("/add-queries", async (req, res) => {
       const result = await queryCollection.find().toArray();
+      res.send(result);
+    });
+
+    //Get all queries posted by a specific user
+    app.get("/queries/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { "buyer.user_email": email };
+      const result = await queryCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //Delete a Specific Query
+    app.delete("/query/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await queryCollection.deleteOne(query);
       res.send(result);
     });
 
