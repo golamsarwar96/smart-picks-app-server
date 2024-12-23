b6c4a755db18dd3d90d02736605ed77127a3877f
Your Name <golamsarwar1896@gmail.com>
2024-12-24 04:48:05 +0600

    Getting a specific post to update. after that we will update using post method

--- index.js
+++ index.js
@@ -53,6 +53,20 @@ async function run() {
       res.send(result);
     });
 
+    //Update a specific query
+    // app.get("/queries/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await queryCollection.findOne(query);
+    //   res.send(result);
+    // });
+    app.get("/query/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await queryCollection.findOne(query);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
